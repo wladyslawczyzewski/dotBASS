0f4d76edac6cb9c7ee6e3fc9ad1226319b8ff66a
Vladyslav Chyzhevskyi <coirius@coirius.com>
2012-08-12 16:48:15 +0300

    init

---
+++ dotBASS/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("dotBASS")]
+[assembly: AssemblyDescription("BASS.dll wrapper")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("dotBASS")]
+[assembly: AssemblyCopyright("Copyright © Vladyslav Chyzhevskyi, 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("40bf8504-ac37-4b0d-bf95-7c42e12340ac")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
