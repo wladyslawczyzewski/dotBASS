c5224f84b1d995611e289a4abbc246a431fec37f
Vladyslav Chyzhevskyi <coirius@coirius.com>
2012-08-12 16:59:20 +0300

    Added initialization

--- dotBASS/BASS/BASS.cs
+++ dotBASS/BASS/BASS.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.InteropServices;
 
 namespace dotBASS.BASS
 {
     public class BASS
     {
+        [DllImport(@"bass.dll", CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool BASS_Init(int device, int freq, BASSInitFlags flags, IntPtr win, IntPtr clsid);
+
+        public static bool BASS_Init(int device, int freq, BASSInitFlags flags, IntPtr win)
+        {
+            return BASS_Init(device, freq, flags, win, IntPtr.Zero);
+        }
     }
 }
---
+++ dotBASS/BASS/BASSInitFlags.cs
@@ -0,0 +1,16 @@
+﻿namespace dotBASS.BASS
+{
+    public enum BASSInitFlags
+    {
+        BASS_DEVICE_DEFAULTS = 0,
+        BASS_DEVICE_8BITS = 1,
+        BASS_DEVICE_MONO = 2,
+        BASS_DEVICE_3D = 4,
+        BASS_DEVICE_LATENCY = 256,
+        BASS_DEVICE_CPSPEAKERS = 1024,
+        BASS_DEVICE_SPEAKERS = 2048,
+        BASS_DEVICE_NOSPEAKER = 4096,
+        BASS_DEVICE_DMIX = 8192,
+        BASS_DEVICE_FREQ = 16384
+    }
+}
